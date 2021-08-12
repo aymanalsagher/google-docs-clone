ac2a75c8488bff428136efdafedec1190dc3b31d
aymanalsagher <aymanalsagher@gmail.com>
2021-08-12 17:22:07 +0200

    fixing for netlify v0.2

--- next.config.js
+++ next.config.js
@@ -2,4 +2,24 @@ module.exports = {
   images: {
     domains: ["ssl.gstatic.com", "i.pinimg.com"],
   },
+  plugins: [
+    //ignore the drivers you don't want. This is the complete list of all drivers -- remove the suppressions for drivers you want to use.
+    new FilterWarningsPlugin({
+      exclude: [
+        /mongodb/,
+        /mssql/,
+        /mysql/,
+        /mysql2/,
+        /oracledb/,
+        /pg/,
+        /pg-native/,
+        /pg-query-stream/,
+        /react-native-sqlite-storage/,
+        /redis/,
+        /sqlite3/,
+        /sql.js/,
+        /typeorm-aurora-data-api-driver/,
+      ],
+    }),
+  ],
 };
